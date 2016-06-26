3c6ae93e41ae4008684cc642f744428d0818f3c0
Jonas Anso <jonas.anso@gmail.com>
2016-06-26 20:54:16 +0100

    Name every request independently and make clearer how the end of the scenario is reached.

--- src/it/scala/org/pagination/PaginationSimulation.scala
+++ src/it/scala/org/pagination/PaginationSimulation.scala
@@ -10,29 +10,36 @@ import scala.concurrent.duration._
 
 class PaginationSimulation extends Simulation {
 
-  val httpConf = http.baseURL("https://raw.githubusercontent.com/jonasanso/gatling-pagination/master")
-
+  private val STOP = "stop"
+  private val httpConf = http.baseURL("https://raw.githubusercontent.com/jonasanso/gatling-pagination/master")
 
   def paginate = scenario("Pagination Simulation")
     .exec(
-      paginatedRequest("/data/first.json"))
-    .asLongAs(condition = session => session("next").as[String] != "end", "page", exitASAP = true) {
-      paginatedRequest("${next}")
+      paginatedRequest("/data/first.json", "first"))
+    .asLongAs(condition = nextUrlFound, "page") {
+      paginatedRequest("${next}", "next ${page} ")
     }
 
 
-  private def paginatedRequest(url:Expression[String]): ChainBuilder = {
+  private def paginatedRequest(url:Expression[String], name: Expression[String]): ChainBuilder = {
     exec(
-      http("paginate")
+      http(name)
         .get(url)
         .check(status.is(200))
-        .check(jsonPath("$..meta.next").transformOption(extract => extract.orElse(Some("end")).success)
+        .check(
+          jsonPath("$.meta.next")
+            .transformOption(extract =>
+              extract.orElse(Some(STOP)).success)
         .saveAs("next"))
-    ).pause(20.seconds)
+    ).pause(2.seconds)
   }
 
+  private def nextUrlFound(session: Session) =
+    session("next").as[String] != STOP
+
+
   setUp(
-    paginate.inject(rampUsersPerSec(1) to 3 during 1.minute)
+    paginate.inject(rampUsersPerSec(1) to 3 during 10.seconds)
   ).protocols(httpConf)
 
 }
