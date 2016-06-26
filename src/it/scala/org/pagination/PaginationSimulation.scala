90dae82eb9c04e78f0dc465adf564a43e94f303f
Jonas Anso <jonas.anso@gmail.com>
2016-06-26 19:36:25 +0100

    Simplified and fixed missing .

--- src/it/scala/org/pagination/PaginationSimulation.scala
+++ src/it/scala/org/pagination/PaginationSimulation.scala
@@ -11,21 +11,19 @@ import scala.concurrent.duration._
 class PaginationSimulation extends Simulation {
 
   val baseUrl = "https://raw.githubusercontent.com/jonasanso/gatling-pagination/master"
-  val minUsersPerSecond = 1
-  val maxUsersPerSecond = 5
-  val testDuration = 5.minute
-
   val httpConf = http
     .baseURL(baseUrl)
 
 
   def paginate = scenario("Pagination Simulation")
-    exec(
+    .exec(
       paginatedRequest("/data/first.json"))
     .asLongAs(condition = session => session("next").as[String] != "end", "page", exitASAP = true) {
       paginatedRequest("${next}")
     }
 
+
+  def k = scenario("l")
 
   private def paginatedRequest(url:Expression[String]): ChainBuilder = {
     exec(
@@ -33,12 +31,13 @@ class PaginationSimulation extends Simulation {
         .get(url)
         .check(status.is(200))
         .check(jsonPath("$..meta.next").transformOption(extract => extract.orElse(Some("end")).success)
-          .saveAs("next"))
-    )
+        .saveAs("next"))
+
+    ).pause(10.seconds)
   }
 
   setUp(
-    paginate.inject(rampUsersPerSec(minUsersPerSecond) to maxUsersPerSecond during testDuration)
+    paginate.inject(rampUsersPerSec(1) to 5 during 1.minute)
   ).protocols(httpConf)
 
 }
